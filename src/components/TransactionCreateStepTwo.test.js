9ff16de53775d81c7b3cb4ebeb9b29577aa501fb
aniebodavid <aniebovictor001@gmail.com>
2022-10-10 15:37:51 -0700

    Setip unit test for TransactionCreateStepTwo

---
+++ src/components/TransactionCreateStepTwo.test.js
@@ -0,0 +1,8 @@
+import { screen, render } from "@testing-library/react";
+import TransactionCreateStepTwo from "./TransactionCreateStepTwo";
+
+test("on initial render, the pay button is disabled", () => {
+  render(<TransactionCreateStepTwo sender={{ id: "5" }} receiver={{ id: "5" }} />);
+
+  screen.debug();
+});
