f7e584a10176f0ca5212f39e093562283618e4bd
aniebodavid <aniebovictor001@gmail.com>
2022-10-10 16:40:02 -0700

    add test to check if button is disabled

--- src/components/TransactionCreateStepTwo.test.js
+++ src/components/TransactionCreateStepTwo.test.js
@@ -1,8 +1,8 @@
 import { screen, render } from "@testing-library/react";
 import TransactionCreateStepTwo from "./TransactionCreateStepTwo";
 
-test("on initial render, the pay button is disabled", () => {
+test("on initial render, the pay button is disabled", async () => {
   render(<TransactionCreateStepTwo sender={{ id: "5" }} receiver={{ id: "5" }} />);
 
-  screen.debug();
+  expect(await screen.findByRole("button", { name: /pay/i })).toBeDisabled();
 });
